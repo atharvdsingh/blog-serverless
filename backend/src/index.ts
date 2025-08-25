4075a99b466928f6a7b7a35aacf4b255807c76ba
atharvdsingh <dsingh9450492600@gmail.com>
2025-08-26 00:56:23 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,37 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
 
-app.post('/api/v1/user/signup',(c)=>{
+
+const app = new Hono <{ Bindings:{
+  DATABASE_URL:string
+}}> ()
+
+
+
+app.post('/api/v1/user/signup',async (c)=>{
+  
+  const prisma=new PrismaClient({
+    
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body= await c.req.json()
+  const data=await prisma.users.create({
+    data:{
+      email:body.email,
+      password:body.password
+    }
+
+  })
+  
+
+  
+
+
   return c.text('signup routes')
 
 })
