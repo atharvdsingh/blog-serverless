181c57a98a073569bdd659ad73c4fe76c568ae2b
atharvdsingh <dsingh9450492600@gmail.com>
2025-08-26 19:29:14 +0530

    done with user login and sign up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,28 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from 'hono/adapter'
+ import {  sign ,decode} from 'hono/jwt'
 
 
 
 
 const app = new Hono <{ Bindings:{
-  DATABASE_URL:string
+  DATABASE_URL:string,
+  JWT_SECRET:string
 }}> ()
 
 
+app.use('/api/v1/user/*',async (c,next)=>{
+  const body=await c.req.json()
+  if(!body.token){
+    return c.json({"message":"does not containet toeken"})
+  }
+
+
+  next()
+
+})
 
 app.post('/api/v1/user/signup',async (c)=>{
   
@@ -20,22 +32,60 @@ app.post('/api/v1/user/signup',async (c)=>{
   }).$extends(withAccelerate())
 
   const body= await c.req.json()
-  const data=await prisma.users.create({
+
+  const data=await prisma.users.findUnique({
+    where:{
+      email:body.email
+    }
+  })
+  if(data){
+    return c.json({"message":"user already exist"})
+  }
+
+  const user=await prisma.users.create({
     data:{
       email:body.email,
       password:body.password
     }
-
   })
+
+  if(!user){
+    throw console.error('user is not created');
+    
+  }
+  const token= await sign({id:user.id},c.env.JWT_SECRET)
+
+  return c.json({jwt:token,data:"user is created"})
   
 
   
 
 
-  return c.text('signup routes')
+  
 
 })
-app.post('/api/v1/user/singin',(c)=>{
+app.post('/api/v1/user/singin', async (c)=>{
+
+    const prisma = new PrismaClient({
+      datasourceUrl:c.env.DATABASE_URL
+    }).$extends(withAccelerate())
+
+    const body = await c.req.json()
+    
+    const user= await prisma.users.findUnique({
+      where:{
+        email:body.email,
+        password:body.password
+      }
+    })
+
+    if(!user){
+      return c.json({"message":"user does not exit"})
+    }
+    const token=await sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({jwt:token})
+
+   
   return c.text('signup routes')
 
 })
